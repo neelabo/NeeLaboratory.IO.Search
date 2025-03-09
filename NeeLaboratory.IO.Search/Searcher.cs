53c136fbcabd93fe1c8eac870f2628ac57b9640f
Ito Mitsuhiro <nee.laboratory@gmail.com>
2025-03-09 16:28:20 +0900

    feat: フィルター機能を切り出せるようにした

--- NeeLaboratory.IO.Search/Searcher.cs
+++ NeeLaboratory.IO.Search/Searcher.cs
@@ -54,7 +54,7 @@ namespace NeeLaboratory.IO.Search
         /// <param name="keyword"></param>
         /// <returns></returns>
         /// <exception cref="SearchKeywordException">フォーマットエラー</exception>
-        public IEnumerable<SearchKey> Analyze(string keyword)
+        public List<SearchKey> Analyze(string keyword)
         {
             return _searchKeyAnalyzer.Analyze(keyword);
         }
@@ -103,36 +103,97 @@ namespace NeeLaboratory.IO.Search
             {
                 return all;
             }
-
-            // 定義キーを追加
-            var fixedKeys = PreKeys.Concat(keys).Concat(PostKeys);
+            
+            // フィルター生成
+            var units = CreateFilterCollection(keys);
 
             // キーワードによる絞込
-            foreach (var key in fixedKeys)
+            foreach (var unit in units)
             {
                 token.ThrowIfCancellationRequested();
 
-                var filter = key.Filter.CreateFunc(key.Property, key.PropertyParameter, key.Format);
-
-                switch (key.Conjunction)
+                switch (unit.Key.Conjunction)
                 {
                     case SearchConjunction.And:
-                        entries = entries.Where(e => filter.IsMatch(_context, e, token));
+                        entries = entries.Where(e => unit.Filter.IsMatch(_context, e, token));
                         break;
                     case SearchConjunction.Or:
-                        entries = entries.Union(all.Where(e => filter.IsMatch(_context, e, token)));
+                        entries = entries.Union(all.Where(e => unit.Filter.IsMatch(_context, e, token)));
                         break;
                     case SearchConjunction.Not:
-                        entries = entries.Where(e => !filter.IsMatch(_context, e, token));
+                        entries = entries.Where(e => !unit.Filter.IsMatch(_context, e, token));
                         break;
                     case SearchConjunction.PreOr:
-                        entries = all.Where(e => filter.IsMatch(_context, e, token)).Union(entries);
+                        entries = all.Where(e => unit.Filter.IsMatch(_context, e, token)).Union(entries);
                         break;
                 }
             }
 
             return entries;
         }
+
+        public List<SearcherFilterUnit> CreateFilterCollection(IEnumerable<SearchKey> keys)
+        {
+            var fixedKeys = PreKeys.Concat(keys).Concat(PostKeys);
+
+            return  fixedKeys
+                .Select(key => new SearcherFilterUnit(key, key.Filter.CreateFunc(key.Property, key.PropertyParameter, key.Format)))
+                .ToList();
+        }
+
+        public SearcherFilter CreateFilter(string keyword)
+        {
+            var units = string.IsNullOrEmpty(keyword) ? new() : CreateFilterCollection(Analyze(keyword));
+            return new SearcherFilter(_context, units);
+        }
     }
 
+
+    /// <summary>
+    /// 検索のフィルター機能を切り出したもの
+    /// </summary>
+    public class SearcherFilter
+    {
+
+        private readonly SearchContext _context;
+        private readonly List<SearcherFilterUnit> _units;
+
+        public SearcherFilter(SearchContext context) : this(context, new())
+        {
+        }
+
+        public SearcherFilter(SearchContext context, List<SearcherFilterUnit> units)
+        {
+            _context = context;
+            _units = units;
+        }
+
+        public bool Filter(ISearchItem entry)
+        {
+            var result = true;
+
+            foreach (var unit in _units)
+            {
+                switch (unit.Key.Conjunction)
+                {
+                    case SearchConjunction.And:
+                        result = result && unit.Filter.IsMatch(_context, entry, CancellationToken.None);
+                        break;
+                    case SearchConjunction.Or:
+                        result = result || unit.Filter.IsMatch(_context, entry, CancellationToken.None);
+                        break;
+                    case SearchConjunction.Not:
+                        result = result && !unit.Filter.IsMatch(_context, entry, CancellationToken.None);
+                        break;
+                    case SearchConjunction.PreOr:
+                        result = unit.Filter.IsMatch(_context, entry, CancellationToken.None) || result;
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public record SearcherFilterUnit(SearchKey Key, SearchFilter Filter);
 }
